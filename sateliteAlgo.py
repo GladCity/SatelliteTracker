a1f4ef8ee2ff346bf20f99f965679ea7e16f6b22
QVolodya <102404345+QVolodya@users.noreply.github.com>
2023-10-27 11:08:50 +0300

    Converters completed

--- sateliteAlgo.py
+++ sateliteAlgo.py
@@ -2,13 +2,16 @@ import math
 
 earthWeight = 5.97 * 10 ** 24
 graviConst = 6.67 * 10 ** -11
-earthRadius = 6,378 * 10 ** 6
+earthRadius = 6.378 * 10 ** 6
+
 
 def calcSateliteSpeed(sateliteHeight):
     return (graviConst * earthWeight / (earthRadius + sateliteHeight))
 
+
 def calcSatelitePeriod(sateliteHeight):
     return (2 * math.pi * (earthRadius + sateliteHeight) / calcSateliteSpeed(sateliteHeight))
+
 
 def fromGeodetToGeocent(LBHlist):
     x = (earthRadius + LBHlist[2]) * math.cos(LBHlist[1]) * math.cos(LBHlist[0])
@@ -16,21 +19,31 @@ def fromGeodetToGeocent(LBHlist):
     z = (earthRadius + LBHlist[2]) * math.sin(LBHlist[1])
     return [x, y, z]
 
+
 def fromGeocentToGeodet(XYZlist):
     x = XYZlist[0]
     y = XYZlist[1]
     z = XYZlist[2]
     rd = earthRadius
     r = (x ** 2 + y ** 2) ** 0.5
-    e2 = 1 / rd ** 2
+    e2 = math.e ** 2
+    et2 = 1 / rd ** 2
     F = 54 * rd ** 2 * z ** 2
     G = r ** 2 + (1 - e2) * z ** 2
     c = e2 ** 2 * F * r ** 2 / G ** 3
     s = (1 + c + (c ** 2 + 2 * c) ** 0.5) ** (1 / 3)
     P = F / (3 * (s + 1 / s + 1) ** 2 * G ** 2)
     Q = (1 + 2 * e2 ** 2 * P) ** 0.5
-    r0 = -(P * e2 * r) / (1 - Q) + (1 / 2 * rd ** 2 * (1 + 1 / Q) - P(1 - e2) * z ** 2 / (Q * (1 + Q)) - 1 / 2 * P + r ** 2) ** 0.5
-
+    r0 = -(P * e2 * r) / (1 - Q) + (1 / 2 * rd ** 2 * (1 + 1 / Q)
+                                    - P(1 - e2) * z ** 2 / (Q * (1 + Q))
+                                    - 1 / 2 * P + r ** 2) ** 0.5
+    U = ((r - e2 * r0) ** 2 + z ** 2) ** 0.5
+    V = ((r - e2 * r0) ** 2 + (1 - e2) * z ** 2) ** 0.5
+    z0 = rd * z / V
+    H = U * (1 - rd / V)
+    B = math.atan((z + et2 * z0) / r)
+    L = math.atan2(y, x)
+    return [L, B, H]
 
 
 
