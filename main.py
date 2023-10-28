ee2b610f904a6db6de335d7d2e1488d56aa7b64f
Sava <sitnikovaiq@yandex.ru>
2023-10-28 14:12:10 +0300

    some changes

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route('/check-area', methods=['POST'])
 def check_area():
-    jsons = json.loads(request.json)
+    print(request.json)
+    jsons = json.loads(request.json)["features"][0]["geometry"]["coordinates"][0]
     min_l = [1000., 0]
     max_l = [-1000., 0]
     latitude = []
@@ -27,9 +28,11 @@ def check_area():
     out = {}
     for i in satellites:
         res = sateliteAlgo.calcRequiredTimeAndSatTrack(i.norad_id, i.photo_size, [min_l, max_l])
-        if res[0] is None:
+        print(res)
+        if res is None or res[0] is None:
             continue
-        out[i.name] = {"track": res[0], "time": res[1], "price": area * i.price, "resolution": i.details}
+        out[i.name] = {"time": res[1], "price": area * i.price, "resolution": i.details}
+    print(out)
     return json.dumps(out)
 
 
@@ -43,6 +46,7 @@ def get_satellite():
     return json.dumps(out)
 
 
-
 if __name__ == '__main__':
     app.run(debug=True, host="192.168.8.100", port=8080)
+
+# [[70.05096087892255, 45.507857244004754], [70.05096087892255, 37.355714171633025], [79.8807678807745, 37.355714171633025], [79.8807678807745, 45.507857244004754], [70.05096087892255, 45.507857244004754]]
